880a00160b450ae535600ce7810ae701b778e7f1
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-10-23 12:52:17 +0200

    fixed bug in 'move highlighted block' remap

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -5,7 +5,7 @@ vim.keymap.set('c', '<C-B>', '<Left>')
 
 -- move and indent highlighted text blocks:
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 -- don't move cursor when joining lines:
 vim.keymap.set('n', 'J', 'mzJ`z')

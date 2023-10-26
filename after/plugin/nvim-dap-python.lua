bbbbd71db5bc4fd8937dfaf126c09dfe017a5835
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-10-26 22:50:21 +0200

    remaps for running tests in *debugger*

--- after/plugin/nvim-dap-python.lua
+++ after/plugin/nvim-dap-python.lua
@@ -1,1 +1,9 @@
 require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+require('dap-python').test_runner = 'pytest'
+
+-- debug/test/method:
+vim.keymap.set('n', '<leader>dtm', ':lua require("dap-python").test_method()<CR>')
+-- debug/test/class:
+vim.keymap.set('n', '<leader>dtc', ':lua require("dap-python").test_class()<CR>')
+
+

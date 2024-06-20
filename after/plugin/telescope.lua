69bc5ca44551578522aede957516e209f03b5ff7
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2024-06-20 22:58:12 +0200

    <leader>do for diagnostics overlay window

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,4 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.keymap.set('n', '<leader>dd', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float, {})

6eee136d285202c32f687070383e13133b8dc18c
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2024-08-02 00:55:47 +0200

    luasnips
    for python function and for rst figure.

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,8 +1,10 @@
-local snip = require('luasnip')
-local s = snip.snippet
+local ls = require('luasnip')
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
 
-snip.config.set_config({
+ls.config.set_config({
     -- keep the last snippet around so we can jump back into it:
     history = true,
 
@@ -10,13 +12,53 @@ snip.config.set_config({
     enable_autosnippets = true,
 })
 
-snip.add_snippets('all', {
+ls.add_snippets('all', {
     -- available in any filetype
-
     -- shebang
     s('#!', fmt(
         [[#!/usr/bin/env python]] .. '\n' ..
         [["""]] .. '\n' ..
         [["""]], {})
     ),
+})
+
+ls.add_snippets('python', {
+    -- python function:
+    -- trigger is "def":
+	s('def', {
+		t({ 'def ' }),
+		-- placeholder for function's name (with initial text):
+		i(1, 'function_name'),
+		t('('),
+		-- Placeholder for arguments (with initial text):
+		i(2, 'arg1: int'),
+		-- type hint for return value:
+		t({ ') -> ' }),
+        i(3, 'int'),
+        t({ ':', '' }),
+		-- docstring:
+		t({ '\t"""', '\t' }),
+        -- later, we may do some automatic docstring magic here
+        t({ '', '\t"""', '', '\t' }),
+		-- Last placeholder, for function body. Exit point of the snippet.
+		i(0),
+	}),
+})
+
+ls.add_snippets('rst', {
+    -- figure:
+    -- trigger is ".. figure::":
+	s('.. figure::', {
+		t({ '.. figure:: ' }),
+		-- placeholder for filename (with initial text):
+		i(1, 'filename'),
+		t({ '', '    :alt: ' }),
+		-- Placeholder for alternate text (with initial text):
+		i(2, '<alternate text>'),
+		-- width:
+		t({ '', '    :width: 80%', '', '    ' }),
+        -- caption (with initial text)::
+        i(0, '<caption>'),
+        t({ '', '' }),
+	}),
 })
--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -41,10 +41,20 @@ vim.keymap.set('t', '<C-[>', '<C-\\><C-n>')
 
 -- oil.nvim:
 vim.keymap.set('n', '-', '<cmd>Oil<CR>', { desc = 'open parent directory' })
---
+
 -- luasnip:
-local ls = require("luasnip")
-vim.keymap.set({"i"}, "<C-k>", function() ls.expand() end, {silent = true})
+local ls = require('luasnip')
 
 vim.keymap.set('n', '<leader>ls',
                '<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>')
+
+vim.keymap.set({'i'}, '<C-K>', function() ls.expand() end, {silent = true})
+vim.keymap.set({'i', 's'}, '<C-L>', function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({'i', 's'}, '<C-J>', function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({'i', 's'}, '<C-E>', function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
+

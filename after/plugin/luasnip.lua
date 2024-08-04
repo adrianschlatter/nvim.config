3ca7e076068b83df85a7e823b4d7c84854cee756
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2024-08-04 23:43:04 +0200

    luasnip python def
    Now writes numpy-style docstrings for me based on arguments and return
    type.

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,7 +1,43 @@
 local ls = require('luasnip')
 local s = ls.snippet
+local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
 local fmt = require('luasnip.extras.fmt').fmt
+local format = string.format
+
+local function docstring_params(args, parent, user_args)
+    local res = {}
+
+    -- args has the form { { 'arg1: int, arg2: str, arg3: int' } }
+    local name_type_pairs = vim.split(args[1][1], ',')
+    -- iterate over the list of pairs
+    for _, pair in ipairs(name_type_pairs) do
+        -- split pair into tuple of name and type
+        local parts = vim.split(pair, ':')
+        -- create docstring and add to res
+        if #parts == 2 then
+            table.insert(res, format('\t%s : %s', vim.trim(parts[1]),
+                         vim.trim(parts[2])))
+            table.insert(res, '\t\t<XXX>')
+            table.insert(res, '')
+        elseif #parts == 1 then
+            table.insert(res, format('\t%s :', vim.trim(parts[1])))
+            table.insert(res, '\t\t<XXX>')
+            table.insert(res, '')
+        end
+    end
+
+    -- remove the last empty line
+    table.remove(res, #res)
+
+    return res
+end
+
+local function docstring_returns(args, parent, user_args)
+    return {'\t' .. args[1][1],
+            '\t\t<XXX>'}
+end
 
 ls.config.set_config({
     -- keep the last snippet around so we can jump back into it:
@@ -31,11 +67,29 @@ ls.add_snippets('python', {
         [[
             def {}({}) -> {}:
                 """
+                <XXX>
+
+                Parameters
+                ----------
+
+            {}
+
+                Returns
+                -------
+
+            {}
+
+                Raises
+                ------
+
+                XXXException
+                    <If XXX goes wrong.>
                 """
 
                 {}
         ]],
-        {i(1, 'function_name'), i(2, 'arg1: int'), i(3, 'int'), i(0, 'pass')})
+        {i(1, 'function_name'), i(2, 'arg1: int'), i(3, 'int'),
+         f(docstring_params, {2}), f(docstring_returns, {3}), i(0, 'pass')})
     ),
 })
 

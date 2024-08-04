84f5ec8948a14e32c3a622b7a0c03e2b5dfff018
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2024-08-04 20:46:42 +0200

    Cleaner snippets by using fmt()

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,6 +1,5 @@
 local ls = require('luasnip')
 local s = ls.snippet
-local t = ls.text_node
 local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
 
@@ -13,52 +12,44 @@ ls.config.set_config({
 })
 
 ls.add_snippets('all', {
-    -- available in any filetype
-    -- shebang
-    s('#!', fmt(
-        [[#!/usr/bin/env python]] .. '\n' ..
-        [["""]] .. '\n' ..
-        [["""]], {})
+    -- shebang:
+    s({trig = '#!'},
+      fmt(
+        [[
+            #!/usr/bin/env python
+            """
+            """
+        ]],
+        {})
     ),
 })
 
 ls.add_snippets('python', {
     -- python function:
-    -- trigger is "def":
-	s('def', {
-		t({ 'def ' }),
-		-- placeholder for function's name (with initial text):
-		i(1, 'function_name'),
-		t('('),
-		-- Placeholder for arguments (with initial text):
-		i(2, 'arg1: int'),
-		-- type hint for return value:
-		t({ ') -> ' }),
-        i(3, 'int'),
-        t({ ':', '' }),
-		-- docstring:
-		t({ '\t"""', '\t' }),
-        -- later, we may do some automatic docstring magic here
-        t({ '', '\t"""', '', '\t' }),
-		-- Last placeholder, for function body. Exit point of the snippet.
-		i(0),
-	}),
+    s({trig = 'def'},
+      fmt(
+        [[
+            def {}({}) -> {}:
+                """
+                """
+
+                {}
+        ]],
+        {i(1, 'function_name'), i(2, 'arg1: int'), i(3, 'int'), i(0, 'pass')})
+    ),
 })
 
 ls.add_snippets('rst', {
     -- figure:
-    -- trigger is ".. figure::":
-	s('.. figure::', {
-		t({ '.. figure:: ' }),
-		-- placeholder for filename (with initial text):
-		i(1, 'filename'),
-		t({ '', '    :alt: ' }),
-		-- Placeholder for alternate text (with initial text):
-		i(2, '<alternate text>'),
-		-- width:
-		t({ '', '    :width: 80%', '', '    ' }),
-        -- caption (with initial text)::
-        i(0, '<caption>'),
-        t({ '', '' }),
-	}),
+    s({trig = 'fig'},
+      fmt(
+        [[
+        .. figure:: {}
+            :alt: {}
+            :width: 80%
+
+            {}
+        ]],
+        {i(1, 'filename'), i(2, '<alternate text>'), i(0, '<caption>')})
+    ),
 })
